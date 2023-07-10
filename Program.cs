f6cc8f3bf048a1e971c24d10556934a1edbb9219
Matthe815 <matthew.elswick@matthe815.dev>
2023-07-10 15:07:05 -0400

    refactor: Fix tool selector namespace and add it

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace MHFQuestEditor
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new QuestSelector());
+            Application.Run(new ToolSelector());
         }
     }
 }
